2d5cc5d2ae14fdc8ed9e9a404c4f0cb08968ce25
Ashirdeveloper <162281442+Ashirdeveloper@users.noreply.github.com>
2025-08-29 11:30:38 -0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
     const handleOnSubmit=async (e) => {
         e.preventDefault();
         try {
-            const response= await axios.post('https://e-commerce-crud-backend.vercel.app//auth/login', value)
+            const response= await axios.post('https://e-commerce-crud-backend.vercel.app/auth/login', value)
             const datas = response.data
             console.log(datas)
         if (response.status==200){
