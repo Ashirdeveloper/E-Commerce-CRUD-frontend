3c9ae37d76ec27ecc6f2ea78d4602d48a557767f
Ashirdeveloper <162281442+Ashirdeveloper@users.noreply.github.com>
2025-08-29 14:09:21 -0700

    Update EditModal.jsx

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -14,9 +14,6 @@ const EditModal = ({item}) => {
     const [description,setDescription] = useState('');
     const [imageUrl,setImageUrl] = useState('');
     const {Auth} = useSelector((state) => state.Auth)// Access the nested Auth property
-    const handleOnClick= () => {
-    window.location.reload();
-     };
     const handleOnSubmit=async (e) => {
             e.preventDefault();
             try {
@@ -65,7 +62,7 @@ const EditModal = ({item}) => {
                     </div>
                 </div>
                 <div className="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                    <button data-modal-toggle="crud-modal" type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleOnClick}>Update</button>
+                    <button data-modal-toggle="crud-modal" type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Update</button>
                     <button data-modal-toggle="crud-modal" type="submit" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete</button>
                 </div>
             </form>
