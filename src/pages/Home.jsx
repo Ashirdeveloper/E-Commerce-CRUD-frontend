ebf2809e71dbdaad733f0afe8d9b589134235d08
Ashirdeveloper <162281442+Ashirdeveloper@users.noreply.github.com>
2025-08-29 11:45:48 -0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,6 +42,8 @@ const Home = () => {
   return (
     <>
       <Navbar />
+      <CustomModal />
+      <EditModal item={productID} />
     <div className="min-h-screen w-full bg-gray-200 p-8">
       <div className="text-center mb-10">
         <h1 className="text-3xl font-bold text-gray-800">
@@ -56,8 +58,6 @@ const Home = () => {
           ))}
         </div>
       </div>
-          <CustomModal />
-    <EditModal item={productID} />
     </div>
     </>
   )
